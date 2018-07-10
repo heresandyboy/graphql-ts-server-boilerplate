b771851c2a426ce67981ed159233d88c738fc811
Andy Ainsworth <andy.ainsworth@zeninternet.co.uk>
2018-07-10 15:26:50 +0100

    Setup some bad tests - will get better later

---
+++ src/tests/register.test.ts
@@ -0,0 +1,32 @@
+import { request } from "graphql-request";
+import { host } from "./constants";
+import { createConnection } from "typeorm";
+import { User } from "../entity/User";
+
+const email = "tom@bob.com";
+const password = "jalksdf";
+
+const mutation = `
+mutation {
+  register(email: "${email}", password: "${password}")
+}
+`;
+// these are a simple / bad way to attempt integration style tests of the server = will update with better approach later
+test("Register user", async () => {
+  const response = await request(host, mutation);
+  expect(response).toEqual({ register: true });
+
+  await createConnection(); // do before test
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).not.toEqual(password);
+});
+
+// rules
+
+// use a test database
+// drop all data once the test is over
+// when I run yarn test it should start a server ?? -- unless can stop it as mentioned in the comments
